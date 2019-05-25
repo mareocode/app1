61f84b594c006689fd466c10dca2b9b926aa937d
MRUIZ <mruiz@mruiz.imac.com>
2019-05-25 14:07:29 -0400

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
@@ -8,14 +10,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './components/items/items.component';
 import { ItemService } from "./services/item.service";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase, 'app1'),
     AngularFirestoreModule
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { Item } from '../models/Item';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,33 @@ import { Item } from '../models/Item';
 export class ItemService {
   itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
+  itemDoc: AngularFirestoreDocument<Item>;
 
   constructor(public afs: AngularFirestore) {
-    this.items = this.afs.collection('items').valueChanges();
-    // this.items = this.afs.collection('items').snapshotChanges().map(changes => {
-    //   return changes.map(a => {
-    //     const data = a.payload.doc.data() as Item;
-    //     data.id = a.payload.doc.id;
-    //     return data;
-    //   });
-    // });
+    // this.items = this.afs.collection('items').valueChanges();
+    this.itemsCollection = this.afs.collection('items', ref => ref.orderBy('title', 'asc'));
+
+    this.items = this.itemsCollection.snapshotChanges().pipe(map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+    });
+    }));
+
    }
 
    getItems(){
      return this.items;
+   }
+
+   addItem(item: Item){
+    this.itemsCollection.add(item);
+   }
+
+   deleteItem(item: Item){
+    this.itemDoc = this.afs.doc(`items/${item.id}`);
+    
    }
 
 }
